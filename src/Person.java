93b2c496100efe1f84108a8e1e69c88760006dcc
BoglaevYaromir <iboglaev14@gmail.com>
2026-02-11 21:29:50 +0300

    Сражение двух случайных персонажей

---
+++ src/Person.java
@@ -0,0 +1,111 @@
+public class Person {
+    public Person(String name){
+        this.name = name;
+        strength = (short) (Math.random()*96 +5); //максимальная сила 100, минимальная 5, нужна для урона, также определяет здоровье и скорость
+        health = (short) (strength*10+Math.random()*strength*6-strength*3); //расчитывается как сила*10 +- до 30% силы, максимальное здоровье 1300, минимальное 35
+        dexterity = (short) (Math.random()*200+1); //максимальная гибкость 200, используется при вычислении шанса уворота
+        energy = (short) (Math.random()*10000+1); //максимальная энергия 10_000, нужна для урона и защиты
+        iq = (short) (Math.random()*251 + 50); //максимальный IQ 300, минимальный 50, нужен для шанса уворота
+        speed = (short) (Math.random()*201 + strength/2); //максимальная скорость 250, минимальная 2, нужна для урона и шанса уворота
+        reaction = (short) (Math.random()*200+1); //максимальная реакция 200, нужна для шанса уворота
+        mentalHealth = (short) ((health + strength + (energy/10) + iq + speed + reaction)/6);
+        damage = (short)(strength + energy/100 + speed/10); //максимальный урон 225, минимальный 2
+    }
+
+    private final String name;
+    private short health;
+    private short mentalHealth;
+    private short strength;
+    private short dexterity;
+    private short iq;
+    private short speed;
+    private short reaction;
+    private short energy;
+    private short damage;
+
+    public void printCharacter(){ //вывод всей информации
+        System.out.printf("Name = %s, Health = %d, Mental Health = %d, Strength = %d, IQ = %d, Speed = %d, Reaction = %d, Energy = %d\n", name, health,
+                mentalHealth, strength, iq, speed, reaction, energy);
+    }
+
+    public void printHealthAndEnergy(){ //вывод здоровья и энергии
+        System.out.printf("Name = %s, Health = %d, Energy = %d\n",name, health, energy);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public short getHealth(){
+        return health;
+    }
+    public short getMentalHealth(){
+        return mentalHealth;
+    }
+
+    public short getStrength() {
+        return strength;
+    }
+
+    public short getDexterity() {
+        return dexterity;
+    }
+
+    public short getIq() {
+        return iq;
+    }
+
+    public short getSpeed() {
+        return speed;
+    }
+
+    public short getEnergy() {
+        return energy;
+    }
+
+    public short getReaction() {
+        return reaction;
+    }
+
+    public short getDamage() {
+        return damage;
+    }
+
+    public void setHealth(short health){
+        this.health = health;
+    }
+
+    public void setReaction(short reaction) {
+        this.reaction = reaction;
+    }
+
+    public void setEnergy(short energy) {
+        this.energy = energy;
+    }
+
+    public void setIq(short iq) {
+        this.iq = iq;
+    }
+
+    public void setMentalHealth(short mentalHealth) {
+        this.mentalHealth = mentalHealth;
+    }
+
+    public void setSpeed(short speed) {
+        this.speed = speed;
+    }
+
+    public void setStrength(short strength) {
+        this.strength = strength;
+    }
+
+    public void setDexterity(short dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public void setDamage() {
+        damage = (short)(strength + energy/100 + speed/10);
+    }
+
+
+}
